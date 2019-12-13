0d187a322e7154698469bc84c4b15d51a0399708
Adarsh <adarsh.bhautoo@gmail.com>
2019-12-13 12:01:05 +0400

    [Codes] - Update of sitecore forms

---
+++ Px.SitecoreForms.Demo/ExecuteActions.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.ExperienceForms.Mvc.Pipelines.ExecuteSubmit;
+using Sitecore.Mvc.Pipelines;
+using System;
+using System.Collections.Generic;
+
+namespace Px.SitecoreForms.Demo
+{
+    public class ExecuteActions : MvcPipelineProcessor<ExecuteSubmitActionsEventArgs>
+    {
+        public override void Process(ExecuteSubmitActionsEventArgs args)
+        {
+            Assert.ArgumentNotNull((object)args, nameof(args));
+            foreach (ParameterizedSubmitAction submitAction in (IEnumerable<ParameterizedSubmitAction>)args.SubmitActions)
+            {
+                if (args.FormSubmitContext.Canceled)
+                    break;
+                submitAction.SubmitAction.ExecuteAction(args.FormSubmitContext, submitAction.Parameters);
+            }
+        }
+    }
+}
